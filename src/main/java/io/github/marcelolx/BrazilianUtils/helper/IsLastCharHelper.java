712f007ef1849439f1ecf5c3f96c8ce579aa79fe
Marcelo Lauxen <marcelolauxen16@gmail.com>
2019-04-27 17:04:43 -0300

    Cria helper para verificar se o índice é o último da string

---
+++ src/main/java/io/github/marcelolx/BrazilianUtils/helper/IsLastCharHelper.java
@@ -0,0 +1,9 @@
+package io.github.marcelolx.brazilianutils.helper;
+
+public class IsLastCharHelper {
+	
+	public static Boolean verify(Integer index, String str) {
+		return index == str.length() - 1;
+	}
+	
+}
